306073c81f0b0f19196cdc7e1321a536a220f3ff
Tomy52 <toto9116@gmail.com>
2024-10-09 10:05:52 -0300

    Agregar clase Persona

---
+++ src/Persona.java
@@ -0,0 +1,43 @@
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private int dni;
+
+    public Persona(String nombre, String apellido, int dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public int getDni() {
+        return this.dni;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String toString() {
+        return "Nombre: " + this.nombre + "\nApellido: " + this.apellido + "\nDNI: " + this.dni;
+    }
+
+    public void mostrar() {
+        System.out.println(this.toString());
+    }
+}
